5b04886d35d864e7233030f345ff915b84dc503e
TCON163 <tylerconnercareersearch@gmail.com>
2021-10-11 17:54:36 -0500

    built model and started servelet

---
+++ Project 1/Back End/back-end/src/main/java/servlets/DependencyLoaderListener.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+
+public class DependencyLoaderListener  implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
