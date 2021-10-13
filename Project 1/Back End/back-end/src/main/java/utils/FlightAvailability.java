e1df804acc7b836bd43a9d92c68ce9e0e4ff9925
BrittLowell <brittany.lowell@revature.net>
2021-10-13 12:52:03 -0600

    Added comment to util class

--- Project 1/Back End/back-end/src/main/java/utils/FlightAvailability.java
+++ Project 1/Back End/back-end/src/main/java/utils/FlightAvailability.java
@@ -5,6 +5,7 @@ public class FlightAvailability {
     public FlightAvailability() {
     }
 
+    //Checks if a customer can still purchase tickets for a flight
     public static boolean availableFlights(){
         boolean isAvailable = true;
         //TODO: Write code after implementing Hibernate
