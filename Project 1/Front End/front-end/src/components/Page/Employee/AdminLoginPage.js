1d02c8f93b3548b193e5100800dc94c60bed33d3
BrittLowell <brittany.lowell@revature.net>
2021-10-15 23:39:48 -0600

    Attempting to combine the admin and pilot login pages

--- Project 1/Front End/front-end/src/components/Page/Employee/AdminLoginPage.js
+++ Project 1/Front End/front-end/src/components/Page/Employee/AdminLoginPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './EmployeeLoginPage.css';
 import AdminHomePage from './AdminHomePage';
-import { render } from '@testing-library/react';
+import PilotHomePage from './PillotHomePage';
 
 function AdminLoginPage() {
     /*Create variables for username and password and initializing them with the useState hook. Provides the current 
@@ -14,52 +14,100 @@ function AdminLoginPage() {
     const [isLoggedIn, setIsLoggedIn] = useState("");
     const [id, setId] = useState("");
     const showPassword = false;
+    let employeeRole;
 
     //Setting minimum and maximum username and password lengths. Does not check if the username and password match.
     function validateForm() {
         return username.length >= 3 && username.length <= 20 && password.length >= 6 && password.length <= 20;
     }
 
-    async function loginUser(credentials) {
+        async function loginAdmin(credentials) {
 
 
-        return await fetch('http://localhost:8080/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-
-            },
-            body: JSON.stringify(credentials)
+            return await fetch('http://localhost:8080/api/admin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+    
+                },
+                body: JSON.stringify(credentials)
+            }
+            ).then((response) => response.json()
+            ).then(data => {
+                setId(data["id"])
+                setRealUsername(data["username"])
+                setFirstName(data["firstName"])
+                setLastName(data["lastName"])
+                setIsLoggedIn(true)
+            }).catch(error => console.log(error));
+    
         }
-        ).then((response) => response.json()
-        ).then(data => {
-            setId(data["id"])
-            setRealUsername(data["username"])
-            setFirstName(data["firstName"])
-            setLastName(data["lastName"])
-            setIsLoggedIn(true)
-        }).catch(error => console.log(error));
+    
+        function renderAdminHomePage () {
+            if(isLoggedIn){
+                return(
+                    <AdminHomePage />
+                );
+            }
+        
+            }
+        
+        async function loginPilot(credentials) {
 
-    }
 
-    function renderAdminHomePage () {
-        if(isLoggedIn){
-            return(
-                <AdminHomePage />
-            );
+            return await fetch('http://localhost:8080/api/pilot', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+    
+                },
+                body: JSON.stringify(credentials)
+            }
+            ).then((response) => response.json()
+            ).then(data => {
+                setId(data["id"])
+                setRealUsername(data["username"])
+                setFirstName(data["firstName"])
+                setLastName(data["lastName"])
+                setIsLoggedIn(true)
+            }).catch(error => console.log(error));
+    
         }
-
-    }
+    
+        function renderPilotHomePage () {
+            if(isLoggedIn){
+                return(
+                    <PilotHomePage />
+                );
+            }
+    
+        }
 
     async function handleSubmit(e) {
         e.preventDefault();
-        await loginUser({
-            username: username,
-            password: password
-        });
-        renderAdminHomePage();
+        console.log(employeeRole);
+        if(employeeRole === 0){
+            await loginAdmin({
+                username: username,
+                password: password
+            });
+            renderAdminHomePage();
+        } else if(employeeRole === 1) {
+            await loginPilot({
+                username: username,
+                password: password
+            });
+        }
     }
 
+    function userRole(role){
+        if(role == 'Admin'){
+            employeeRole = 0;
+        } else if(role == 'Pilot'){
+            employeeRole = 1;
+        }
+        return employeeRole;
+    }
 
     //Creating the header and the login form. Login button is disabled if an invalid username or password is entered.
     return (
@@ -76,6 +124,15 @@ function AdminLoginPage() {
                 </label>
                 <button type="submit" disabled={!validateForm()}>Login</button>            
             </form>
+            <div className="Role">
+                <button onClick="userRole('Admin')">
+                    Admin
+                </button>
+                <div className="divider" />
+                <button onClick="userRole('Pilot')">
+                    Pilot
+                </button>
+            </div>
         </div>
     );
 }
