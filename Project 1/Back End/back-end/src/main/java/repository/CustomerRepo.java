5d1ed5a081320b71e12e11fd434e98f9e5fd965a
BrittLowell <brittany.lowell@revature.net>
2021-10-16 08:50:44 -0600

    adding repos

--- Project 1/Back End/back-end/src/main/java/repository/CustomerRepo.java
+++ Project 1/Back End/back-end/src/main/java/repository/CustomerRepo.java
@@ -1,4 +1,8 @@
 package repository;
 
+import org.hibernate.Session;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
 public class CustomerRepo implements GenericRepo{
 }
--- Project 1/Back End/back-end/src/main/java/repository/GenericRepo.java
+++ Project 1/Back End/back-end/src/main/java/repository/GenericRepo.java
@@ -1,4 +1,18 @@
 package repository;
 
+import org.hibernate.Session;
+
 public interface GenericRepo {
+
+    static void init(Session s) {
+    }
+
+    static List<E> getAll() {
+        return null;
+    }
+
+    static List<E> getItemById(){
+        return null;
+    }
+
 }
