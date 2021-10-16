64fd094949744e5e002f9e5fcf939fcaeaa96e7d
TCON163 <tylerconnercareersearch@gmail.com>
2021-10-16 10:11:43 -0500

    forgot I commented out a lot of code trying to find the hibernate errors.

--- Project 1/Back End/back-end/src/main/java/servlets/FlightServlet.java
+++ Project 1/Back End/back-end/src/main/java/servlets/FlightServlet.java
@@ -16,31 +16,31 @@ import java.util.List;
 public class FlightServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-//        Flight one = new Flight(1,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 50);
-//        Flight two = new Flight(2,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 50);
-//        Flight three = new Flight(3,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 50);
-//        Flight four = new Flight(4,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 60);
-//        Flight five = new Flight(5,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 65);
-//        Flight six = new Flight(6,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 55);
-//        Flight seven = new Flight(7,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 75);
-//        Flight eight = new Flight(8,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 70);
-//
-//        List<Flight> list = new ArrayList<>();
-//        list.add(one);
-//        list.add(two);
-//        list.add(three);
-//        list.add(four);
-//        list.add(five);
-//        list.add(six);
-//        list.add(seven);
-//        list.add(eight);
-//
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        String json = mapper.writeValueAsString(list);
-//        resp.getWriter().write(json);
-//
+        Flight one = new Flight(1,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 50);
+        Flight two = new Flight(2,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 50);
+        Flight three = new Flight(3,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 50);
+        Flight four = new Flight(4,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 60);
+        Flight five = new Flight(5,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 65);
+        Flight six = new Flight(6,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 55);
+        Flight seven = new Flight(7,"ATL","MOB", "10/15/2021", 7, "10/15/2021", 8, false, 75);
+        Flight eight = new Flight(8,"MOB","ATL", "10/15/2021", 7, "10/15/2021", 8, false, 70);
+
+        List<Flight> list = new ArrayList<>();
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        list.add(four);
+        list.add(five);
+        list.add(six);
+        list.add(seven);
+        list.add(eight);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String json = mapper.writeValueAsString(list);
+        resp.getWriter().write(json);
+
 
 
     }
--- Project 1/Back End/back-end/src/main/java/servlets/LoginServlet.java
+++ Project 1/Back End/back-end/src/main/java/servlets/LoginServlet.java
@@ -18,52 +18,52 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
-//
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        String requestData = req.getReader().lines().collect(Collectors.joining());
-//        Customer customer = mapper.readValue(requestData,Customer.class);
-//        System.out.println(requestData);
-//
-//        String customerJsonString;
-//
-//        System.out.println("You are logged in!");
-//
-//        //create a customer obj while logged in.
-//        try {
-//            Connection conn = ConnectionManager.getConnection();
-//            CustomerDAO dao = new CustomerDAO(conn);
-//            Customer c = dao.customerLogin(customer.getUsername(),customer.getPassword());
-//            customerJsonString = mapper.writeValueAsString(c);
-//
-//            if (c != null) {
-//
-//
-//                resp.setContentType("application/json");
-//                resp.setCharacterEncoding("UTF-8");
-//                System.out.println(customerJsonString);
-//                resp.getWriter().write(customerJsonString);
-//
-//            }else {
-//
-//                resp.setStatus(404);
-//                resp.setContentType("application/json");
-//                resp.setCharacterEncoding("UTF-8");
-//                PrintWriter out = resp.getWriter();
-//                out.print("User name is incorrect.");
-//                System.out.println("Invalid login.");
-//            }
-//        } catch (SQLException throwables) {
-//            throwables.printStackTrace();
-//            resp.setStatus(404);
-//            resp.setContentType("application/json");
-//            resp.setCharacterEncoding("UTF-8");
-//            PrintWriter out = resp.getWriter();
-//            out.print("User name is incorrect.");
-//
-//
-//        }
-//
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        String requestData = req.getReader().lines().collect(Collectors.joining());
+        Customer customer = mapper.readValue(requestData,Customer.class);
+        System.out.println(requestData);
+
+        String customerJsonString;
+
+        System.out.println("You are logged in!");
+
+        //create a customer obj while logged in.
+        try {
+            Connection conn = ConnectionManager.getConnection();
+            CustomerDAO dao = new CustomerDAO(conn);
+            Customer c = dao.customerLogin(customer.getUsername(),customer.getPassword());
+            customerJsonString = mapper.writeValueAsString(c);
+
+            if (c != null) {
+
+
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("UTF-8");
+                System.out.println(customerJsonString);
+                resp.getWriter().write(customerJsonString);
+
+            }else {
+
+                resp.setStatus(404);
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("UTF-8");
+                PrintWriter out = resp.getWriter();
+                out.print("User name is incorrect.");
+                System.out.println("Invalid login.");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            resp.setStatus(404);
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            PrintWriter out = resp.getWriter();
+            out.print("User name is incorrect.");
+
+
+        }
+
 
 
 
