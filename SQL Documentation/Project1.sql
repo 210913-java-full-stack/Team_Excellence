aeb2bd2bee26e8ae7732e80f32c12693a62e6162
BrittLowell <brittany.lowell@revature.net>
2021-10-16 13:26:18 -0600

    Fixed merge conflicts

--- SQL Documentation/Project1.sql
+++ SQL Documentation/Project1.sql
@@ -84,9 +84,17 @@ CONSTRAINT ticket_to_customer FOREIGN KEY (customer_id) REFERENCES customers (cu
 ################## TEST CREATING A USER ##################
 ###########################################################
 
-INSERT INTO customers (username, password, email, first_name, last_name) VALUES ("TCON", "password", "tyler.conner@revature.net", "Tyler", "Conner");
-INSERT INTO customers (username, password, email, first_name, last_name) VALUES ("Britt", "password", "brittany.lowell@revature.net", "Brittany", "Lowell");
-INSERT INTO admins (username, password, email, first_name, last_name) VALUES ("Britt","password", "brittany.lowell@revature.net", "Brittany", "Lowell");
+INSERT INTO pilots(username, password, email, first_name, last_name) VALUES ("Britt","password","brittany.lowell@revature.net", "Brittany", "Lowell");
+INSERT INTO flights(pilot_id,depart,arrive,depart_time,depart_date,arrive_time,arrive_date,available,take_off) VALUES (1, "DIA", "LAX","12:30","2021-10-20","11:30","2021-10-20",true,false);
+INSERT INTO customers(customer_id,username, password, email, first_name, last_name) VALUES (1,"TCON", "password", "tyler.conner@revature.net", "Tyler", "Conner");
+INSERT INTO customers(customer_id,username, password, email, first_name, last_name) VALUES (2,"Britt", "password", "brittany.lowell@revature.net", "Brittany", "Lowell");
+INSERT INTO admins(admin_id,username, password, email, first_name, last_name) VALUES (1,"Britt","password", "brittany.lowell@revature.net", "Brittany", "Lowell");
+INSERT INTO tickets(flight_id,customer_id,passenger_first_name,passenger_last_name,passenger_age,checked_in) VALUES (1, 1, "Tyler", "Conner", 30, true);
+INSERT INTO tickets(flight_id,customer_id,passenger_first_name,passenger_last_name,passenger_age,checked_in) VALUES (1, 2, "Brittany", "Lowell", 26, false);
+
 SELECT * FROM customers;
 SELECT * FROM admins;
+SELECT * FROM pilots;
+SELECT * FROM flights;
+SELECT * FROM tickets;
 
