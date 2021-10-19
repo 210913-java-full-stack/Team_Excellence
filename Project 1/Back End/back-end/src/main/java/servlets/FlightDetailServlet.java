e2ec1e9f1c673f11561168f6929be9bbf21299ed
BrittLowell <brittany.lowell@revature.net>
2021-10-19 12:12:28 -0600

    checking that FlightDetailServlet matches main

--- Project 1/Back End/back-end/src/main/java/servlets/FlightDetailServlet.java
+++ Project 1/Back End/back-end/src/main/java/servlets/FlightDetailServlet.java
@@ -5,6 +5,7 @@ import model.Flight;
 import repository.FlightRepo;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
