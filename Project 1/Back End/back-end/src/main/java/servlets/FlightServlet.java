a8ab0d4486c0dc82703a3170cbea12a130c92e90
TCON163 <tylerconnercareersearch@gmail.com>
2021-10-20 15:58:09 -0500

    save Changes

--- Project 1/Back End/back-end/src/main/java/servlets/FlightServlet.java
+++ Project 1/Back End/back-end/src/main/java/servlets/FlightServlet.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class FlightServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        List<Flight> list = FlightRepoWHibernateUtil.getAllFlights();
+        List<Flight> list = FlightRepo.getAllFlights();
         System.out.println("DEBUG: " + list.get(0).getFlightId());
         System.out.println("This this the contents of the list");
         for(int i = 0; i < list.size(); i++){
