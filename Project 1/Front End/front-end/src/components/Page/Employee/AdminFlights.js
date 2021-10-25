1a790c63aea838a4811bef2d216d2dad5da12a21
TCON163 <tylerconnercareersearch@gmail.com>
2021-10-25 17:30:58 -0500

    fixed table design

--- Project 1/Front End/front-end/src/components/Page/Employee/AdminFlights.js
+++ Project 1/Front End/front-end/src/components/Page/Employee/AdminFlights.js
@@ -64,56 +64,63 @@ export default function AdminFlights({ controlCreatePage, controlFlightPage, set
             <h1>Flight List</h1>
             <button className="createPage" onClick={clickCreatePage}>Create New Flight</button>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-                            Flight Id
-                        </th>
-                        <th>Departure</th>
-                        <th>Departur Date</th>
-                        <th>Departure Time</th>
-                        <th>Arrival</th>
-                        <th>Arrival Date</th>
-                        <th>Arrival Time</th>
-                        <th>Taken Off</th>
-                    </tr>
-                </thead>
+            <div id="table-wrapper">
+                <div id="table-scroll">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    Flight Id
+                                </th>
+                                <th>Departure</th>
+                                <th>Departur Date</th>
+                                <th>Departure Time</th>
+                                <th>Arrival</th>
+                                <th>Arrival Date</th>
+                                <th>Arrival Time</th>
+                                <th>Taken Off</th>
+                            </tr>
+                        </thead>
 
-                <tbody >
-
-
-                    {
-                        flights.map(el => {
-                            return (
-
-                                <tr key={el.flightId}>
-
-                                    <td><button className="flight-button" onClick={e => clickFlightButton(e.target.value)} value={el.flightId}>BCON  {el.flightId}</button></td>
-                                    <td>{el.departLocation}</td>
-                                    <td>{el.departDate}</td>
-                                    <td>{el.departTime}</td>
-                                    <td>{el.arriveLocation}</td>
-                                    <td>{el.arriveDate}</td>
-                                    <td>{el.arriveTime}</td>
-                                    {departed(el.takeOff)}
+                        <tbody >
 
 
-                                </tr>
+                            {
+                                flights.map(el => {
+                                    return (
 
-                            )
-                        })
-                    }
+                                        <tr key={el.flightId}>
+
+                                            <td><button className="flight-button" onClick={e => clickFlightButton(e.target.value)} value={el.flightId}>BCON  {el.flightId}</button></td>
+                                            <td>{el.departLocation}</td>
+                                            <td>{el.departDate}</td>
+                                            <td>{el.departTime}</td>
+                                            <td>{el.arriveLocation}</td>
+                                            <td>{el.arriveDate}</td>
+                                            <td>{el.arriveTime}</td>
+                                            {departed(el.takeOff)}
 
 
+                                        </tr>
 
-                </tbody>
+                                    )
+                                })
+                            }
 
 
 
+                        </tbody>
 
 
-            </table>
+
+
+
+                    </table>
+
+                </div>
+
+            </div>
+
 
         </section>
     );
