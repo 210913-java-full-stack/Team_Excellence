a3649d63b5b1288c07e6e0319dd2e2a2065bb18e
BrittLowell <brittany.lowell@revature.net>
2021-10-25 17:31:57 -0600

    changes

--- Project 1/Front End/front-end/src/components/Page/Customer/CheckIn.js
+++ Project 1/Front End/front-end/src/components/Page/Customer/CheckIn.js
@@ -3,46 +3,21 @@ import "./CheckIn.css";
 
 function CheckIn() {
 
-    const [ticketId, setTicketId] = useState('');
+    const [ticketId, setTicketId] = useState()
 
 
     async function handleSubmit() {
-        alert("ticket ID: ", ticketId);
-        let response = await fetch(`http://localhost:8080/api/ticket?ticketId=${ticketId}`, {
-            method: "PUT"
-        })
-        alert("ticket ID: ", ticketId);
-        await respAlert(response);    
-        alert("ticket ID: ", ticketId);  
-
-        // fetch(`http://localhost:8080/api/ticket?ticketId=${ticketId}`, {
-        //     method: "PUT"
-        // }).then((response) => {
-        //     if (response.ok) {
-        //         alert("You have checked In!")
-        //     } else {
-        //         alert("That's an invalid ticket number!")
-        //     }
-        // })
-    }
-
-    function respAlert(resp) {
-        console.log(resp);
-        alert(resp);
-        if(resp.ok) {
-            alert("logged in!");
-        } else {
-            alert("That's an invalid ticket number!");
-        }
-    }
-
-    function testChange(e) {
-
-        console.log("event: ", e.target.value);
-        setTicketId(e.target.value);
-        console.log("state: ", ticketId);
+        console.log("ticket ID: ", ticketId);
         
-        alert("test");
+        fetch(`http://localhost:8080/api/ticket?ticketId=${ticketId}`, {
+            method: "PUT"
+        }).then((response) => {
+            if (response.ok) {
+                alert("You have checked In!")
+            } else {
+                alert("That's an invalid ticket number!")
+            }
+        })
     }
 
     return (
@@ -55,7 +30,7 @@ function CheckIn() {
                         <label>Enter your ticket number:</label>
                     </div>
                     <div>
-                        <input type="text" value={ticketId} onChange={(e) => testChange(e)} required ></input>
+                        <input type="text" onChange={e => setTicketId(e.target.value)} required ></input>
                     </div>
                     <button className="ticket-button" type="submit">Check In</button>
                 </form>
